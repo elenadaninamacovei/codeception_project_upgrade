cc4500a5bfb9883d5b98ae1ba3a99a152ff17235
elenadaninamacovei <daninamacovei@gmail.com>
2025-02-19 18:10:42 +0200

    example of successful & unsuccessful login tests

--- tests/Acceptance/LoginCest.php
+++ tests/Acceptance/LoginCest.php
@@ -20,4 +20,18 @@ class LoginCest {
         $I->seeInCurrentUrl('/inventory.html');
         $I->see('Products', '.title');
     }
+
+    public function invalidUsername(AcceptanceTester $I)
+    {
+        $I->wantToTest('Login with an invalid username should display an error message and stay on the homepage');
+        $I->amOnPage('/');
+
+        $I->amGoingTo('Insert invalid credentials & submit');
+        $I->fillField('#user-name', 'invalid_user');
+        $I->fillField('#password', 'secret_sauce');
+        $I->click('#login-button');
+
+        $I->seeCurrentUrlEquals('/');
+        $I->see('Epic sadface: Username and password do not match any user in this service', '.error-message-container.error');
+    }
 }
